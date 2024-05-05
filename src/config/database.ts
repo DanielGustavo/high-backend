12791ce04039ddd40ee932ff7b099b995868fc18
Daniel Gustavo <dg520589@gmail.com>
2024-05-05 15:57:02 -0300

    fix: fix database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,5 +3,5 @@ export default {
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT || 5432),
-  host: process.env.DB_PORT,
+  host: process.env.DB_HOST,
 };
